03ff946e64b43d857db5102e29d1be55f1ae6cec
Christopher Wood <christopherwood07@gmail.com>
2015-07-28 09:45:27 -0700

    template

--- src/cbc.h
+++ src/cbc.h
@@ -1,14 +1,30 @@
 
 
+(CBCParameters, CBCMasterKey) = Setup(parameter)
+CBCSecretKey = KeyGen(CBCMasterKey, CBCPublicIndex)
+CBCEncryptedPayload = Encrypt(CBCPublicParameters, CBCInput)   
+CBCOutput = Decrypt(CBCSecretKey, CBCEncryptedPayload)    
+// y is the desired output of the function
+
 
 CBCIdentity
+
 CBCPublicKey
 CBCPrivateKey
+CBCMasterKey
+CBCParameters
+CBCIndex
+
 CBCKeyGenerator
+
+CBCBuffer
+CBCSignature
+
 CBCSigner
 CBCVerifier
 CBCEncryptor
 CNCDecryptor
+
 CBCScheme
 
 
