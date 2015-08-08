9dadd7f390c47fe5f869d4a2c6277070937817a5
Christopher Wood <christopherwood07@gmail.com>
2015-08-07 21:36:30 -0700

    test

--- src/test/test.c
+++ src/test/test.c
@@ -6,6 +6,8 @@ main(int argc, char** argv)
 	DummyEncryptionScheme *scheme = dummyCreate(1);
 	DummyParameters *params = dummySetup(2);
 
+	// Create a generic type using the concrete instances and the right interface
+
 	// CBCMasterKey *msk = cbcGenerateMasterKey(scheme, params);
 
     return 0;
