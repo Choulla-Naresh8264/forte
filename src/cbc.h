47c727c618238580ead300dc5695d49dea854bca
Christopher Wood <christopherwood07@gmail.com>
2015-09-29 09:32:52 -0700

    Stripped the main file and header

--- src/cbc.h
+++ src/cbc.h
@@ -4,10 +4,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-struct cbc_blob;
-typedef struct cbc_blob CBCBlob;
-
-CBCBlob *createBlob(size_t length, uint8_t input[length]);
-void blobDisplay(CBCBlob *output);
+// ?...
 
 #endif /* libcbc_h_ */
